6d4eb9482c97687e9fbe75a49d76ee044dcf4f56
Luminaar <kovykmax@gmail.com>
2022-03-04 10:45:24 +0100

    List commits without git

--- src/convbump/git.py
+++ src/convbump/git.py
@@ -3,8 +3,9 @@ import subprocess
 from dataclasses import dataclass
 from operator import itemgetter
 from pathlib import Path
-from typing import Iterable, Iterator, List, Optional, Tuple
+from typing import List, Optional, Tuple
 
+from dulwich.objects import Commit as RawCommit
 from dulwich.repo import Repo
 from semver import VersionInfo as Version
 
@@ -34,29 +35,62 @@ class Commit:
     body: Optional[str]
 
 
+def parse_message(message: str) -> Tuple[str, str]:  # Tuple[subject, message]
+    """Parse git message into subject and body."""
+
+    paragraphs = message.strip().split("\n\n")
+    maybe_subject = paragraphs[0]
+    if "\n" in maybe_subject:
+        subject = ""
+        body = message
+    else:
+        subject = maybe_subject
+        body = "\n\n".join(paragraphs[1:])
+
+    return subject, body
+
+
 class Git:
     def __init__(self, path: Path) -> None:
         self.path = path
         self.repo = Repo(self.path)  # type: ignore
 
-    def list_commits(self, from_ref: Optional[str], to_ref: Optional[str] = None) -> List[Commit]:
-        def iter_commtis(commit_ids: Iterable[str]) -> Iterator[Commit]:
-            for commit_id in commit_ids:
-                yield self._parse_commits(["git", "log", "-1", r"--format=%h%n%s%n%b", commit_id])
-
-        if from_ref is None:
-            command = ["git", "log", "--format=%H"]
-        elif from_ref and to_ref:
-            command = ["git", "log", "--format=%H", f"{from_ref}..{to_ref}"]
-        else:
-            command = ["git", "log", "--format=%H", f"{from_ref}..HEAD"]
-
+    def list_commits(
+        self, from_ref: Optional[bytes], to_ref: Optional[bytes] = None
+    ) -> List[Commit]:
         try:
-            commit_ids = reversed(self._check_output(command).splitlines())
-        except subprocess.CalledProcessError:
+            walker = self.repo.get_walker(reverse=True)
+        except KeyError:  # Repo is empty
             return []
 
-        return [commit for commit in iter_commtis(commit_ids)]
+        # Convert from_ref to SHA if it is a tag name
+        _, from_sha_or_none = self.repo.refs.follow(from_ref or b"")
+        from_sha = from_sha_or_none if from_sha_or_none else from_ref
+
+        # Convert to_ref to SHA if it is a tag name
+        _, to_sha_or_none = self.repo.refs.follow(to_ref or b"")
+        to_sha = to_sha_or_none if to_sha_or_none else to_ref
+
+        if from_sha is None and to_sha is None:
+            add = True
+        else:
+            add = False
+        commits: List[Commit] = []
+        for entry in walker:
+            commit: RawCommit = entry.commit
+            hash = commit.id
+
+            if add:
+                message = commit.message.decode()
+                subject, body = parse_message(message)
+                commits.append(Commit(hash, subject, body or None))
+
+            if hash == from_sha:
+                add = True
+            if to_ref and hash == to_sha:
+                break
+
+        return commits
 
     def retrieve_last_commit(self) -> str:
         return self._check_output(["git", "log", "-1", "--format=%B"])
