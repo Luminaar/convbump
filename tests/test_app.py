9bca18c9442eb1b4b2da12007f6dc93d231a73b6
Max Kovykov <max.kovykov@gendigital.com>
2024-03-22 18:22:08 +0100

    feat: Implement --ignore-pattern flag

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import pytest
 from conftest import (
     BREAKING_FEATURE_IN_BODY,
@@ -8,7 +10,8 @@ from conftest import (
     GitFactory,
 )
 
-from convbump.__main__ import _run
+from convbump.__main__ import _run, ignore_commit
+from convbump.conventional import CommitType, ConventionalCommit
 from convbump.version import DEFAULT_FIRST_VERSION
 
 
@@ -67,3 +70,111 @@ def test_conventional_commits(create_git_repository: GitFactory) -> None:
     next_version, _ = _run(git, False)
     assert next_version == DEFAULT_FIRST_VERSION.bump_minor()
 
+
+@pytest.mark.parametrize(
+    "patterns, commit, result",
+    [
+        (
+            ["aiohttp"],
+            ConventionalCommit(
+                CommitType.CHORE,
+                "deps",
+                False,
+                "Update aiohttp",
+                "",
+                "",
+                "chore(deps): Update aiohttp",
+            ),
+            True,
+        ),
+        (
+            ["aiohttp"],
+            ConventionalCommit(
+                CommitType.CHORE,
+                "deps",
+                False,
+                "Update deps",
+                "Update aiohttp",
+                "",
+                "chore(deps): Update aiohttp",
+            ),
+            True,
+        ),
+        (
+            ["deps"],
+            ConventionalCommit(
+                CommitType.CHORE,
+                "deps",
+                False,
+                "Update aiohttp",
+                "",
+                "",
+                "chore(deps): Update aiohttp",
+            ),
+            True,
+        ),
+        (
+            ["chore"],
+            ConventionalCommit(
+                CommitType.CHORE,
+                "deps",
+                False,
+                "Update aiohttp",
+                "",
+                "",
+                "chore(deps): Update aiohttp",
+            ),
+            True,
+        ),
+        (
+            ["feat", "aiohttp"],
+            ConventionalCommit(
+                CommitType.CHORE,
+                "deps",
+                False,
+                "Update aiohttp",
+                "",
+                "",
+                "chore(deps): Update aiohttp",
+            ),
+            True,
+        ),
+        (
+            [""],
+            ConventionalCommit(
+                CommitType.CHORE,
+                "deps",
+                False,
+                "Update aiohttp",
+                "",
+                "",
+                "chore(deps): Update aiohttp",
+            ),
+            False,
+        ),
+        (
+            ["formatting"],
+            ConventionalCommit(
+                CommitType.CHORE,
+                "deps",
+                False,
+                "Update aiohttp",
+                "",
+                "",
+                "chore(deps): Update aiohttp",
+            ),
+            False,
+        ),
+    ],
+)
+def test_ignore_commit(patterns: List[str], commit: ConventionalCommit, result: bool) -> None:
+    assert ignore_commit(patterns, commit) is result
+
+
+def test_ignore_prefix(create_git_repository: GitFactory) -> None:
+    git = create_git_repository([(INITIAL_COMMIT, "v0.1.0"), CHORE])
+
+    with pytest.raises(
+        ValueError
+    ):  # The chore commit should be skipped, so there are no new commits left
+        _run(git, False, ignored_patterns=["chore"])
